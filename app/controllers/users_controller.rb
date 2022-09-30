43d626bd28abb5f527c9568ae7b69e6f2ceefff2
Rachel Katz <rachel.katz@flatironschool.com>
2022-09-30 10:35:53 -0400

    commit tree 206b529ecaf8e7b151133a113e1f617669157a0e

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+    #rescue_from ActiveRecord::InvalidRecord, with: :invalid_record_response
+    before_action :authorize, only: [:show]
+    def create 
+        user = User.create(user_params)
+        if user.valid? 
+            session[:user_id] = user.id 
+            render json: user, status: :created 
+        else   
+            render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+
+        end 
+    end 
+
+    def show 
+        user = User.find_by(id: session[:user_id])
+        render json: user 
+    end 
+
+    private 
+
+    def invalid_record_response 
+        render json: {"error": "users controller create"}, status: :unprocessable_entity
+    end 
+
+
+    def authorize 
+        if !session.include? :user_id 
+            return render json: { error: "Not authorized" }, status: :unauthorized
+        end 
+    end 
+
+    def user_params 
+        params.permit(:username, :password, :password_confirmation)
+    end 
+end
